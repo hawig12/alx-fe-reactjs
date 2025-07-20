715cd3ff367f367dfea3437a153bdc034ceeace9
Hawi <nabiee1264@gmail.com>
2025-07-20 19:10:38 +0300

    feat: Apply inline CSS styling to components in alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px', borderRadius: '8px' }}>
+      <p style={{ fontSize: '0.9em' }}>&copy; 2023 City Lovers</p>
+    </footer>
+  );
+}
+export default Footer;
--- alx-react-app-new/src/components/WelcomeMessage.jsx
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -1,12 +1,11 @@
-
-// src/components/Welcome.js
 function WelcomeMessage() {
   return (
     <div>
       <h1>Hello everyone, I am learning React at ALX!</h1>
       <p>This is a simple JSX component.</p>
-      <p>I am learning about JSX!</p> {/* This new tag, with EXACT text */}
+      <p>I am learning about JSX!</p>
     </div>
   );
 }
-// ...
+
+export default WelcomeMessage;
