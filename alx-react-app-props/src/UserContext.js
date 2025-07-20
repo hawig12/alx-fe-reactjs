39ee22c01fb3755490d7e95d483a879e03e0d02d
hawig12 <nabiee1264@gmail.com>
2025-07-20 21:01:39 +0300

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,13 +1,4 @@
 // src/userContext.js
 import React from 'react';
-
-// Initialize a Context.
-// The argument passed to createContext (here, null) is the default value
-// that consumers will receive if they don't have a matching Provider above them
-// in the component tree.
 const UserContext = React.createContext(null);
-
-// Export the created context.
-// This allows other components (like App.jsx and UserDetails.jsx)
-// to import and use this specific context.
 export default UserContext;
