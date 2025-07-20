462b23f95d396000667ae8ca41eadd7a8b45390c
hawig12 <nabiee1264@gmail.com>
2025-07-20 20:41:52 +0300

    Update UserInfo.jsx

--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,8 +1,14 @@
 // src/UserInfo.jsx
 import UserDetails from './UserDetails';
 
-function UserInfo({ userData }) {
-  return <UserDetails userData={userData} />;
+/**
+ * UserInfo component.
+ * This component acts as another intermediary, rendering the UserDetails component.
+ * It no longer receives or passes the 'userData' prop, as data will be
+ * consumed directly from the Context API by the UserDetails component.
+ */
+function UserInfo() { // Removed { userData } from props
+  return <UserDetails />; // Removed userData={userData} from UserDetails
 }
 
 export default UserInfo;
