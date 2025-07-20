6a2f6bca908a86d98577bfceea4843c1f6ec4b43
Hawi <nabiee1264@gmail.com>
2025-07-20 16:34:32 +0300

    fix: Address Welcome.js component checks

---
+++ alx-react-app/src/components/Welcome.js
@@ -0,0 +1,12 @@
+
+// src/components/Welcome.js
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>This is a simple JSX component.</p>
+      <p>I am learning about JSX!</p> {/* This new tag, with EXACT text */}
+    </div>
+  );
+}
+// ...
