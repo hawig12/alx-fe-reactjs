805495abcb6c78bd7794877c97fbf64068736d48
hawig12 <nabiee1264@gmail.com>
2025-07-20 17:57:54 +0300

    Create Header.jsx

---
+++ alx-react-app-new/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    // The style attribute takes a JavaScript object
+    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '1rem', borderRadius: '0.5rem', marginBottom: '1rem' }}>
+      <h1 style={{ margin: 0 }}>My Favorite Cities</h1>
+    </header>
+  );
+};
