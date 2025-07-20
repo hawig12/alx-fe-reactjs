20e41b648e35f517e000f3ce15b97e3d76768b4c
hawig12 <nabiee1264@gmail.com>
2025-07-20 20:25:47 +0300

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,7 +1,6 @@
 // src/UserContext.js
 import React from 'react';
 
-// Initialize a Context and export it
-const UserContext = React.createContext(null); // null as default value
+const UserContext = React.createContext(null);
 
 export default UserContext;
