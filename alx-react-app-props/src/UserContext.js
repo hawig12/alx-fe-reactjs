b8327d46f9eb5cb706d087719e10e7d71e60a8dc
hawig12 <nabiee1264@gmail.com>
2025-07-23 22:00:40 +0300

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,16 +1,6 @@
-import React, { useState } from 'react';
+// src/UserContext.js
+import { createContext } from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0);
+const UserContext = createContext(null);
 
-  return (
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <p>Current Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-    </div>
-  );
-}
-
-export default Counter;
+export default UserContext;
