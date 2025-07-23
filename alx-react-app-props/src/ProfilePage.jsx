2c4ea5963a6976f87f74ed7f9d14301e78d20156
hawig12 <nabiee1264@gmail.com>
2025-07-23 22:10:02 +0300

    Update ProfilePage.jsx

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,15 +1,8 @@
 // src/ProfilePage.jsx
 import UserInfo from './UserInfo';
 
-/**
- * ProfilePage component.
- * This component acts as an intermediary, rendering the UserInfo component.
- * It no longer receives or passes the 'userData' prop, as data will be
- * consumed directly from the Context API by child components.
- */
-function ProfilePage() { // Removed { userData } from props
-  return <UserInfo />; // Removed userData={userData} from UserInfo
+function ProfilePage() {
+  return <UserInfo />;
 }
 
 export default ProfilePage;
-
