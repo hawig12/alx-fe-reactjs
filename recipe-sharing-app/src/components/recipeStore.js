cf413996019b061c9461d2e330afa1dbff6a8623
hawig12 <nabiee1264@gmail.com>
2025-07-25 23:43:44 +0300

    Create recipeStore.js

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,14 @@
+// src/components/recipeStore.js
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+}));
+
+export default useRecipeStore;
+
+
