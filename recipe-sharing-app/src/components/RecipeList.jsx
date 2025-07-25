c424ef183df476e2ab482859fefdd1c7858a8f33
hawig12 <nabiee1264@gmail.com>
2025-07-25 23:50:43 +0300

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -20,3 +20,24 @@ const RecipeList = () => {
 };
 
 export default RecipeList;
+import React from 'react';
+import useRecipeStore from './recipeStore';
+import { Link } from 'react-router-dom';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <ul>
+      {recipes.map((recipe) => (
+        <li key={recipe.id}>
+          <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default RecipeList;
