3792b70ad407649dc037316069cef15910cc1086
hawig12 <nabiee1264@gmail.com>
2025-07-25 23:44:49 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,12 +3,15 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
+  // Adds one recipe
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
+
+  // Replaces the entire recipe list
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
 
 export default useRecipeStore;
-
-
