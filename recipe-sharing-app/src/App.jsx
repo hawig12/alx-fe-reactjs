34113dc9fa6b0a59c82e6650f69b5726288a78a5
hawig12 <nabiee1264@gmail.com>
2025-07-26 13:51:07 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -5,6 +5,40 @@ import AddRecipeForm from './components/AddRecipeForm';
 import EditRecipeForm from './components/EditRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
+
+function App() {
+  return (
+    <Router>
+      <div className="max-w-2xl mx-auto p-4">
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <RecipeList />
+              <FavoritesList />
+              <RecommendationsList />
+            </>
+          } />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/edit/:recipeId" element={<EditRecipeForm />} />
+          <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import EditRecipeForm from './components/EditRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
