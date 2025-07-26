0e75951454a806bc4fdaf488e0cb98810b76ac02
hawig12 <nabiee1264@gmail.com>
2025-07-26 13:37:16 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
 import React from 'react';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
